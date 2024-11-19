01b8c749d6142c888e68914f9eae767fcc93f2f1
NIKIEMAJ <ajulesnikiema@gmail.com>
2024-11-19 15:37:47 +0000

    Dimensions.API

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Text, View, Dimensions } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.box}>
+    <Text style={styles.text} >
+    welcome ! 
+    </Text>
+
+    </View>
     </View>
   );
 }
 
+const windowsWidth = Dimensions.get ("window").width;
+const windowsHeight = Dimensions.get("window").height;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'plum',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box:{
+    width: windowsWidth > 500 ? "70%" : "90%",
+    height: windowsHeight > 600 ? "60%" : "90%",
+    backgroundColor:"lightblue",
+    alignItems:"center",
+    justifyContent:"center"
+  },
+  text:{
+    fontSize : windowsWidth > 500 ? 50 : 24,
+  }
 });
