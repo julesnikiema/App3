8d1fd1fe1115ed94b35c07d78d106565bb8bd3b3
NIKIEMAJ <ajulesnikiema@gmail.com>
2024-11-19 17:17:27 +0000

    DimensionAPIDrawback

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
+import { useState, useEffect } from "react";
 import { StyleSheet,Text, View, Dimensions } from 'react-native';
 
 export default function App() {
+
+  const[dimensions, setDimensions]= useState({
+    window : Dimensions.get("window")
+  })
+
+  useEffect (()=>{
+    const subscription = Dimensions.addEventListener("change", ({window})=>{
+      setDimensions({window});
+    } )
+    return()=> subscription?.remove();
+  })
+
+  const {window} = dimensions;
+  const windowWidth = window.width;
+  const windowHeight = window.height;
+
+
   return (
     <View style={styles.container}>
-    <View style={styles.box}>
-    <Text style={styles.text} >
-    welcome ! 
-    </Text>
-
+    <View style={[styles.box, {width: windowWidth > 500 ? "70%": "90%",
+                              height: windowHeight > 600 ? "60%":"90%"}]}>
+           <Text style={{fontSize: windowWidth> 500 ? 50 : 24}} >
+       welcome jules !
+        </Text>
     </View>
     </View>
   );
 }
 
-const windowsWidth = Dimensions.get ("window").width;
-const windowsHeight = Dimensions.get("window").height;
+// const windowsWidth = Dimensions.get ("window").width;
+// const windowsHeight = Dimensions.get("window").height;
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -24,13 +44,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   box:{
-    width: windowsWidth > 500 ? "70%" : "90%",
-    height: windowsHeight > 600 ? "60%" : "90%",
+
     backgroundColor:"lightblue",
     alignItems:"center",
     justifyContent:"center"
   },
-  text:{
-    fontSize : windowsWidth > 500 ? 50 : 24,
-  }
+  // text:{
+  //   fontSize : windowsWidth > 500 ? 50 : 24,
+  // }
 });
